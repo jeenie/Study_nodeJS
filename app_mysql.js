21cd46a5fe7f7b2fd3ce1798043cd2210b4e57b9
jeenie <jeenie828@gmail.com>
2018-05-17 02:06:13 +0900

    mysql-final

--- app_mysql.js
+++ app_mysql.js
@@ -24,9 +24,7 @@ var app = express();
 app.set('views', './views_mysql');
 app.set('view engine', 'pug');
 app.locals.pretty = true;
-//post 방식으로 들어온 데이터 사용할 때 필요한 모듈
 app.use(bodyParser.urlencoded({ extended : false }));
-//정적인 파일
 app.use('/user', express.static('uploads'));
 
 //router
@@ -39,18 +37,97 @@ app.post('/upload', upload.single('userfile'), function(req, res){
   res.send('Uploaded : ' + req.file.filename);
 });
 
-app.get('/topic/new', function(req, res){
-  fs.readdir('data', function(err, files){
-    //err처리
+app.get('/topic/add', function(req, res){
+  var sql = 'SELECT id, title FROM topic';
+  conn.query(sql, function(err, topics, fields) {
     if(err) {
       console.log(err);
       res.statux(500).send('Internal Server Error');
     }
-    res.render('new', {topics:files});
+    res.render('add', {topics : topics});
   });
 });
 
-//app application(write-save)
+app.post('/topic/add', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
+  conn.query(sql, [title, description, author], function(err, result, fields) {
+    if(err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.redirect('/topic/' + result.insertId);
+    }
+  });
+});
+//Edit post
+app.get(['/topic/:id/edit'], function(req, res) {
+  //Implement a list of posts
+  var sql = 'SELECT id, title FROM topic';
+  conn.query(sql, function(err, topics, fields){
+    var id = req.params.id;
+    //Implment a detailed contents
+    if(id) {
+      var sql = 'SELECT * FROM topic WHERE id = ?';
+      conn.query(sql, [id], function(err, topic, fields) {
+        if(err) {
+          console.log(err);
+          res.statux(500).send('Internal Server Error');
+        } else {
+          res.render('edit', {topics : topics, topic : topic[0]});
+        }
+      });
+    } else {
+      console.log('There is no id.');
+      res.statux(500).send('Internal Server Error');
+    }
+  });
+});
+app.post(['/topic/:id/edit'], function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var id = req.params.id;
+  var sql = 'UPDATE topic SET title=?, description=?, author=? WHERE id=?';
+  conn.query(sql, [title, description, author, id], function(err, result, fields) {
+    if(err){
+      console.log(err);
+      res.statux(500).send('Internal Server Error');
+    } else {
+      res.redirect('/topic/'+id);
+    }
+  });
+});
+//Delete post
+app.get('/topic/:id/delete', function(req, res) {
+  var sql = 'Select id, title From topic';
+  var id = req.params.id;
+  conn.query(sql, function(err, topics, fields) {
+    var sql = 'SELECT * FROM topic WHERE id=?';
+    conn.query(sql, [id],function(err, topic) {
+      if(err) {
+        console.log(err);
+        res.statux(500).send('Internal Server Error');
+      } else {
+          if(topic.length === 0){
+            console.log('There is no record.');
+            res.statux(500).send('Internal Server Error');
+          } else {
+            res.render('delete', {topics:topics, topic:topic[0]});
+          }
+      }
+    });
+  });
+});
+app.post('/topic/:id/delete', function(req, res){
+  var id = req.params.id;
+  var sql = 'DELETE FROM topic WHERE id=?';
+  conn.query(sql, [id], function(err, result) {
+    res.redirect('/topic');
+  });
+});
 app.get(['/topic', '/topic/:id'], function(req, res) {
   //Implement a list of posts
   var sql = 'SELECT id, title FROM topic';
@@ -70,24 +147,6 @@ app.get(['/topic', '/topic/:id'], function(req, res) {
     } else {
       res.render('view', {topics:topics});
     }
-
-  });
-
-});
-
-//기능 : 사용자가 전송한 데이터를 서버에서 받아서
-//      데이터의 제목을 파일명, 본문을 파일의 내용으로 해서 저장
-app.post('/topic', function(req, res){
-  var title = req.body.title;
-  var description = req.body.description;
-  fs.writeFile('data/' + title, description, function(err){
-      if(err) { //만약 err가 있다
-        console.log(err); //console에 에러 출력 -> 사용자에게 직접 브라우저에서 에러를 보여주지 x
-        res.status(500).send('Internal Server Error');
-        //에러가 발생해 위 코드의 send가 실행되면 그 다음 문장 실행 안함.
-        //Success!  출력 안함, Internal Server Error 출력
-      }
-      res.redirect('/topic/' + title);
   });
 });
 
